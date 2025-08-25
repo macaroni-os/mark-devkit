793bba3fa23debefb1bf66b2f045b2ec0bdca6b4
Daniele Rondina <geaaru@macaronios.org>
2025-08-25 17:22:10 +0200

    Prepare v0.22.3

--- pkg/specs/config.go
+++ pkg/specs/config.go
@@ -13,7 +13,7 @@ import (
 const (
 	MARKDEVKIT_CONFIGNAME = "mark-devkit"
 	MARKDEVKIT_ENV_PREFIX = "MARKDEVKIT"
-	MARKDEVKIT_VERSION    = `0.22.2`
+	MARKDEVKIT_VERSION    = `0.22.3`
 )
 
 type MarkDevkitConfig struct {
